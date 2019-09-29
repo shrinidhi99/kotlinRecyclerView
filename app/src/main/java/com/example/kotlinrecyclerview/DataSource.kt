5603c1b3d13b902640781b062abe4dc156be4719
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-09-29 19:05:37 +0530

    updated the DataSource.kt

--- app/src/main/java/com/example/kotlinrecyclerview/DataSource.kt
+++ app/src/main/java/com/example/kotlinrecyclerview/DataSource.kt
@@ -8,75 +8,67 @@ class DataSource {
             val list = ArrayList<BlogPost>()
             list.add(
                 BlogPost(
-                    "Congratulations!",
-                    "You made it to the end of the course!\r\n\r\nNext we'll be building the REST API!",
-                    "https://raw.githubusercontent.com/mitchtabian/Blog-Images/master/digital_ocean.png",
-                    "Sally"
+                    "Dell XPS Thin and Light Laptop-15",
+                    "15.6 4K Touch Display, Intel Core i7-7700HQ, 16 GB RAM, 1 TB SSD, GTX 1050",
+                    "https://images-na.ssl-images-amazon.com/images/I/71xuuIhlLAL._SL1500_.jpg",
+                    "DELL"
                 )
             )
             list.add(
                 BlogPost(
-                    "Time to Build a Kotlin App!",
-                    "The REST API course is complete. You can find the videos here: https://codingwithmitch.com/courses/build-a-rest-api/.",
-                    "https://raw.githubusercontent.com/mitchtabian/Kotlin-RecyclerView-Example/json-data-source/app/src/main/res/drawable/time_to_build_a_kotlin_app.png",
-                    "mitch"
+                    "ASUS ZenBook S13",
+                    "Ultra Thin & Light Laptop 13.9” FHD, Intel Core i7-8565U CPU, GeForce MX150, 8GB RAM",
+                    "https://img-us1.asus.com/A/show/AW000706/2019/0305/AM0000002/201903AM050000002_15517353933494420003814.jpg",
+                    "ASUS"
                 )
             )
 
             list.add(
                 BlogPost(
-                    "Interviewing a Web Developer and YouTuber",
-                    "Justin has been producing online courses for YouTube, Udemy, and his website CodingForEntrepreneurs.com for over 5 years.",
-                    "https://raw.githubusercontent.com/mitchtabian/Kotlin-RecyclerView-Example/json-data-source/app/src/main/res/drawable/coding_for_entrepreneurs.png",
-                    "John"
+                    "Microsoft Surface Laptop (1st Gen) DAL-00001 Laptop",
+                    "(Windows 10 S, Intel Core i7, 13.5 LED-Lit Screen",
+                    "https://images-na.ssl-images-amazon.com/images/I/41erPQLRs5L.jpg",
+                    "MICROSOFT"
                 )
             )
             list.add(
                 BlogPost(
-                    "Freelance Android Developer (Vasiliy Zukanov)",
-                    "Vasiliy has been a freelance android developer for several years. He also has some of the best android development courses I've had the pleasure of taking on Udemy.com.",
-                    "https://raw.githubusercontent.com/mitchtabian/Kotlin-RecyclerView-Example/json-data-source/app/src/main/res/drawable/freelance_android_dev_vasiliy_zukanov.png",
-                    "Steven"
+                    "Acer Swift 7",
+                    "Acer's Swift 7 is the slimmest ultraportable you can buy, a trait that pairs well with its 4G LTE service",
+                    "https://assets.pcmag.com/media/images/510159-the-world-s-thinnest-laptop.jpg?width=810&height=456",
+                    "ACER"
                 )
             )
             list.add(
                 BlogPost(
-                    "Freelance Android Developer, Donn Felker",
-                    "Freelancing as an Android developer with Donn Felker.\\r\\n\\r\\nDonn is also:\\r\\n\\r\\n1) Founder of caster.io\\r\\n\\r\\n2) Co-host of the fragmented podcast (fragmentedpodcast.com).",
-                    "https://raw.githubusercontent.com/mitchtabian/Kotlin-RecyclerView-Example/json-data-source/app/src/main/res/drawable/freelance_android_dev_donn_felker.png",
-                    "Richelle"
+                    "Hands-on with HP's Spectre 13.3",
+                    "The Spectre 13.3 ultraportable laptop sports crazy cabinet hinges and a new logo, and it's thinner than an iPad Pro.",
+                    "https://img.etimg.com/thumb/msid-52865754,width-640,resizemode-4,imgsize-48664/worlds-thinnest-laptop-hp-spectre-13.jpg",
+                    "HP"
                 )
             )
             list.add(
                 BlogPost(
-                    "Work Life Balance for Software Developers",
-                    "What kind of hobbies do software developers have? It sounds like many software developers don't have a lot of hobbies and choose to focus on work. Is that a good idea?",
-                    "https://raw.githubusercontent.com/mitchtabian/Kotlin-RecyclerView-Example/json-data-source/app/src/main/res/drawable/work_life_balance.png",
-                    "Jessica"
+                    "MacBook Air",
+                    "13.3" Retina Display features a 2560 x 1600 screen resolution and a 16:10 aspect ratio for 227 pixels per inch (PPI)",
+                    "https://media.wired.com/photos/5bd883dc5b66a763e54f0b22/191:100/pass/macbookair3.jpg",
+                    "APPLE"
                 )
             )
             list.add(
                 BlogPost(
-                    "Full Stack Web Developer - Nicholas Olsen",
-                    "In this podcast I interviewed the Fullsnack Developer (AKA Nicholas Olsen).\\r\\n\\r\\nNicholas is many things. What I mean by that is, he's good at many things.\\r\\n\\r\\n1. He’s an entrepreneur\\r\\n\\r\\n2. Web developer\\r\\n\\r\\n3. Artist\\r\\n\\r\\n4. Graphic designer\\r\\n\\r\\n5. Musician (drums)\\r\\n\\r\\n6. Professional BodyBuilder.",
-                    "https://raw.githubusercontent.com/mitchtabian/Kotlin-RecyclerView-Example/json-data-source/app/src/main/res/drawable/fullsnack_developer.png",
-                    "Guy"
+                    "Lenovo Ideapad 15.6",
+                    "HD Premium High Performance Laptop, AMD A12-9720P Quad core processor 2.7GHz, 8GB DDR4, 1TB HDD, DVD, Webcam, WiFi,Bluetooth",
+                    "https://images-na.ssl-images-amazon.com/images/I/61hoTcz8p0L._SL1000_.jpg",
+                    "Lenovo"
                 )
             )
             list.add(
                 BlogPost(
-                    "Javascript Expert - Wes Bos",
-                    "Interviewing a web developer, javascript expert, entrepreneur, freelancer, podcaster, and much more.",
-                    "https://raw.githubusercontent.com/mitchtabian/Kotlin-RecyclerView-Example/json-data-source/app/src/main/res/drawable/javascript_expert_wes_bos.png",
-                    "Ruby"
-                )
-            )
-            list.add(
-                BlogPost(
-                    "Senior Android Engineer - Kaushik Gopal",
-                    "Kaushik Gopal is a Senior Android Engineer working in Silicon Valley.\r\n\r\nHe works as a Senior Staff engineer at Instacart.\r\n\r\nInstacart: https://www.instacart.com/",
-                    "https://raw.githubusercontent.com/mitchtabian/Kotlin-RecyclerView-Example/json-data-source/app/src/main/res/drawable/senior_android_engineer_kaushik_gopal.png",
-                    "mitch"
+                    "AORUS 15-W9-7UK0252W Gaming Laptop",
+                    "Intel Core i7-8750H 2.2GHz, 16GB, 2TB HDD, 512GB SSD, 15.6" Full HD 1920 x 1080, NVIDIA RTX 2060 6GB, Windows 10 Home",
+                    "https://img.ebyrcdn.net/910870-877880-800.jpg",
+                    "AORUS"
                 )
             )
             return list
